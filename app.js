e71b7bfe14d44d32aae2fe451d71735e654c5353
rozi123A <likeask59@gmail.com>
2026-04-22 00:18:21 +0100

    Refactor BOT_USERNAME assignment and add Express server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const BOT_USERNAME = process.env.BOT_USERNAME || 'ads_reward123_bot';
+
+const express = require("express");
+const app = express();
+
+const BOT_USERNAME = process.env.BOT_USERNAME ?? "ads_reward123_bot";
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot " + BOT_USERNAME + " is running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
